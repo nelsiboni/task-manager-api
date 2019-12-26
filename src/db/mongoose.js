aaf63f80632ee88a582bd0f516335e4de8b1394c
Nel Siboni <Nel.Siboni@surecomp.com>
2019-12-26 10:58:37 +0200

    delete unused requires in index.

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/task?readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false', {
+mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true
 })
--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 const express = require('express')
 require('./db/mongoose')
-const User = require('./models/user')
-const Task = require('./models/task')
 const userRouter = require('./routers/users')
 const taskRouter = require('./routers/tasks')
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 
 app.use(express.json())
 app.use(userRouter)
